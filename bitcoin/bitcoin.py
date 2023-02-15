385a9007ea527f5ad27d1d0d192f9ae5afae5c6d
yuka_with_data <90426808+yuka-with-data@users.noreply.github.com>
2023-02-15 13:02:40 -0800

    initial commit
    bitcoin program for CS50 Python

---
+++ bitcoin/bitcoin.py
@@ -0,0 +1,45 @@
+import sys
+import requests
+
+# Check Command Line Argument
+def command_line_arg() -> float:
+    if len(sys.argv) < 2:
+        sys.exit("Missing command-line argument")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command-line argument")
+    else:
+        try:
+            n = float(sys.argv[1])
+            print(f"Debug Print {n}")
+            return n
+        except ValueError:
+            sys.exit("Command-line argument is not a number")
+
+# Request API
+def request_api() -> float:
+    try:
+    # save JSON API into r
+        r = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+        # get the JSON content and save it into dict
+        bpi_dict = r.json()
+        # print(bpi_dict)
+        # accessing bpi_dict and retreve rate_float data
+        rate = bpi_dict["bpi"]["USD"]["rate_float"]
+        return rate
+    except requests.RequestException:
+        sys.exit()
+
+# generate price
+def generate_price(n, rate: float):
+    bitcoin_price = n*rate
+    # print(f"${bitcoin_price:,.4f}")
+    return bitcoin_price
+
+def main():
+    n = command_line_arg()
+    rate = request_api()
+    price = generate_price(n, rate)
+    print(f"${price:,.4f}")
+
+if __name__ == "__main__":
+    main()

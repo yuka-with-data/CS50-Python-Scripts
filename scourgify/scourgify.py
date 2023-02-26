06236a6357293cd84464809c55411cb73e0822b1
yuka_with_data <90426808+yuka-with-data@users.noreply.github.com>
2023-02-25 17:33:07 -0800

    Added problem description

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,4 +1,13 @@
 # Problem Set: https://cs50.harvard.edu/python/2022/psets/6/scourgify/
+""" 
+Expects the user to provide two command-line arguments:
+the name of an existing CSV file to read as input, whose columns are assumed to be, in order, name and house, and
+the name of a new CSV to write as output, whose columns should be, in order, first, last, and house.
+Converts that input to that output, splitting each name into a first name and last name. 
+Assume that each student will have both a first name and last name.
+If the user does not provide exactly two command-line arguments, or if the first cannot be read, 
+the program should exit via sys.exit with an error message.
+ """
 
 import sys
 import csv

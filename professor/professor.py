e7e610590c1a3028b9388e6663fd35c258fd5661
yuka_with_data <90426808+yuka-with-data@users.noreply.github.com>
2023-02-26 20:04:08 -0800

    Added problem description

--- professor/professor.py
+++ professor/professor.py
@@ -1,3 +1,16 @@
+# Problem Set: https://cs50.harvard.edu/python/2022/psets/4/professor/
+
+""" 
+Prompts the user for a level, n. If the user does not input 1, 2, or 3, the program should prompt again.
+Randomly generates ten (10) math problems formatted as X + Y = , wherein each of X and Y is a non-negative integer with 
+n digits. No need to support operations other than addition (+).
+Prompts the user to solve each of those problems. 
+If an answer is not correct (or not even a number), the program should output EEE and prompt the user again, 
+allowing the user up to three tries in total for that problem. 
+If the user has still not answered correctly after three tries, the program should output the correct answer.
+The program should ultimately output the user’s score: the number of correct answers out of 10.
+
+ """
 # import libraries
 import random
 
